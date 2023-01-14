a73f6d643a5dad6071959f16043dd2d3db94c963
Vitaly <shishov_84@mail.ru>
2023-01-14 11:47:39 +0300

    added cicle for

--- script.js
+++ script.js
@@ -9,12 +9,31 @@ const personalMovieDB = {
     privat: false,
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    scoreFilm = prompt('На сколько оцените его?', ''),
-    lastFilmTwo= prompt('Один из последних просмотренных фильмов?', ''),
-    scoreFilmTwo= prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastFilm] = scoreFilm;
-personalMovieDB.movies[lastFilmTwo] = scoreFilmTwo;
+for (let i =0; i < 2 ; i++) {
+    let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        scoreFilm = prompt('На сколько оцените его?', '');
+    
+    if (lastFilm != null && scoreFilm != null && lastFilm != '' && scoreFilm != '' && lastFilm.length < 50 ) {
+        
+        personalMovieDB.movies[lastFilm] = scoreFilm;
+        console.log('done');
+
+    } else { 
+        console.log('error');
+        i --;
+    }    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if(personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
